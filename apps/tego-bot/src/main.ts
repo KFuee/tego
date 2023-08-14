3093556bd2bce827f7a5c7e363c289d183dde625
KFuee <jblascoisazac@icloud.com>
2023-08-14 11:59:52 +0200

    feat: instalación e inicialización de discord.js

--- apps/tego-bot/src/main.ts
+++ apps/tego-bot/src/main.ts
@@ -1,1 +1,10 @@
-console.log('Hello World');
+import 'dotenv/config';
+import { Client, GatewayIntentBits } from 'discord.js';
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}`);
+});
+
+client.login(process.env.BOT_SECRET);
