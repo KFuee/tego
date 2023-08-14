9450732519394fc0cdfa73cc6d36a29609478add
KFuee <jblascoisazac@icloud.com>
2023-08-14 12:37:59 +0200

    feat: componente global user-nav y theming orange

--- apps/tego-client/app/page.tsx
+++ apps/tego-client/app/page.tsx
@@ -1,7 +1,15 @@
+import { UserNav } from '@/components/user-nav';
+
 export default async function Index() {
   return (
-    <div>
-      <span>Hola</span>
+    <div className="flex flex-col min-h-screen">
+      <div className="border-b">
+        <div className="flex h-16 items-center px-4">
+          <div className="ml-auto flex items-center">
+            <UserNav />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
